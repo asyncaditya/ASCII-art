138bf92c8c287cbb56a55b485558c06725638e57
asyncaditya <asyncaditya@outlook.com>
2023-07-01 14:35:39 +0530

    initial commit

---
+++ art.py
@@ -0,0 +1,12 @@
+name = """
+PPPP   Y   Y  TTTTT  H   H   OOO   N   N
+P   P   Y Y     T    H   H  O   O  NN  N
+P   P    Y      T    H   H  O   O  N N N
+PPPP     Y      T    HHHHH  O   O  N  NN
+P        Y      T    H   H  O   O  N   N
+P        Y      T    H   H  O   O  N   N
+P        Y      T    H   H   OOO   N   N
+"""
+
+
+print(name)
